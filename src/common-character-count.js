38bc31f13d0e72a1b0eef80bc78c9fb7b436bbb9
Alexandr Yenussov <a.yenussov@gmail.com>
2023-04-12 18:18:07 +0600

    feat: finish common character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(line1, line2) {
+  let commonCount = 0;
+  let maxLength = line1.length >= line2.length ? line1.length : line2.length;
+  const line1Obj = {};
+  const line2Obj = {};
+  for (let i = 0; i < maxLength; i++) {
+    if (line1Obj[line1[i]] == null) {
+      line1Obj[line1[i]] = 1;
+    } else {
+      line1Obj[line1[i]] += 1;
+    }
+    if (line2Obj[line2[i]] == null) {
+      line2Obj[line2[i]] = 1;
+    } else {
+      line2Obj[line2[i]] += 1;
+    }
+  }
+  for (char in line1Obj) {
+    console.log(char);
+    if (Object.keys(line2Obj).includes(char)) {
+      commonCount = commonCount + Math.min(line1Obj[char], line2Obj[char]);
+    }
+  }
+  return commonCount;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
