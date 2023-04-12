eae4f1195625946dfe2984d690bdcf93af314952
Alexandr Yenussov <a.yenussov@gmail.com>
2023-04-12 20:06:03 +0600

    feat: finish encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(line) {
+  let finalLine = "";
+  let matrix = [];
+  for (let i = 0; i < line.length; i++) {
+    if (i === 0 || matrix[matrix.length - 1][0] !== line[i]) {
+      matrix.push([line[i], 1]);
+    } else if (matrix[matrix.length - 1][0] === line[i]) {
+      matrix[matrix.length - 1][1] += 1;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    finalLine = finalLine + matrix[i][1] + matrix[i][0];
+  }
+  finalLine = finalLine.replaceAll("1", "");
+  return finalLine;
 }
 
+encodeLine("aaaatttt");
+
 module.exports = {
-  encodeLine
+  encodeLine,
 };
