f8eb8551daab97fb91364daf812cbd893b089034
Alexandr Yenussov <a.yenussov@gmail.com>
2023-04-17 14:02:56 +0600

    feat: finish extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,57 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+const defaultSeparator = "+";
+const defaultAdditionalSeparator = "|";
 
+function repeater(str, options) {
+  let finalLine;
+  let repeatCycles = options.repeatTimes ?? 1;
+  let matrix = [];
+
+  for (let index = 0; index < repeatCycles; index++) {
+    let line = str;
+
+    if (options.addition !== undefined) {
+      let additions = [];
+      let whatToAdd = "" + options.addition;
+      let repeatTimes = options.additionRepeatTimes ?? 1;
+
+      for (let j = 0; j < repeatTimes; j++) {
+        additions.push(whatToAdd);
+      }
+
+      line += additions.join(options.additionSeparator ?? defaultAdditionalSeparator);
+    }
+
+    matrix.push(line);
+    // console.log(matrix);
+  }
+  finalLine = matrix.join(options.separator ?? defaultSeparator);
+  return finalLine;
+}
+repeater(null, {
+  repeatTimes: 3,
+  separator: "??? ",
+  addition: null,
+  additionRepeatTimes: 3,
+  additionSeparator: "!!!",
+});
+// "nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null"
 module.exports = {
-  repeater
+  repeater,
 };
